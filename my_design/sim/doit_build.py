4a22a115bfc78e285d9d83b192dfee32dd1ef5a0
gatecat <gatecat@ds0.me>
2024-11-20 13:02:26 +0000

    Support building sim on Windows
    Signed-off-by: gatecat <gatecat@ds0.me>

--- my_design/sim/doit_build.py
+++ my_design/sim/doit_build.py
@@ -10,7 +10,12 @@ SOURCE_DIR  = importlib.resources.files("my_design") / "sim"
 RUNTIME_DIR = importlib.resources.files("yowasp_yosys") / "share/include/backends/cxxrtl/runtime"
 
 ZIG_CXX  = f"{sys.executable} -m ziglang c++"
-CXXFLAGS = f"-O3 -g -std=c++17 -Wno-array-bounds -Wno-shift-count-overflow -fbracket-depth=1024"
+if os.name == "nt":
+    CXXFLAGS = f"-O3 -DWIN32 -g -std=c++17 -Wno-array-bounds -Wno-shift-count-overflow -fbracket-depth=1024"
+    LIBS = "-lws2_32"
+else:
+    CXXFLAGS = f"-O3 -g -std=c++17 -Wno-array-bounds -Wno-shift-count-overflow -fbracket-depth=1024"
+    LIBS = ""
 INCLUDES = f"-I {OUTPUT_DIR} -I {SOURCE_DIR}/vendor -I {RUNTIME_DIR}"
 
 
@@ -48,7 +53,7 @@ def task_build_sim():
     return {
         "actions": [
             f"{ZIG_CXX} {CXXFLAGS} {INCLUDES} -o {OUTPUT_DIR}/sim_soc{exe} "
-            f"{OUTPUT_DIR}/sim_soc.cc {SOURCE_DIR}/main.cc {SOURCE_DIR}/models.cc"
+            f"{OUTPUT_DIR}/sim_soc.cc {SOURCE_DIR}/main.cc {SOURCE_DIR}/models.cc {LIBS}"
         ],
         "targets": [
             f"{OUTPUT_DIR}/sim_soc{exe}"
