0ffc7c34fd2ab9540fd2c7928932de7056dd4ae4
Rob Taylor <rob.taylor@chipflow.io>
2025-03-04 14:16:48 +0000

    Fix up software generation for latest amaranth-soc

--- my_design/software/main.c
+++ my_design/software/main.c
@@ -13,7 +13,7 @@ uint32_t spi_xfer(volatile spi_regs_t *spi, uint32_t data, uint32_t width) {
     while (!(spi->status & 0x1)) // wait for rx full
         ;
     spi->config = ((width - 1) << 3) | 0x02; // CS=0, SCK_EDGE=1, SCK_IDLE=0
-    return spi->receive_data; 
+    return spi->receive_data;
 }
 
 void i2c_start(volatile i2c_regs_t *i2c) {
@@ -45,16 +45,6 @@ void i2c_stop(volatile i2c_regs_t *i2c) {
 void main() {
     puts("🐱: nyaa~!\r\n");
 
-    puts("Flash ID: ");
-    puthex(spiflash_read_id(SPIFLASH));
-    puts("\r\n");
-
-    puts("Entering QSPI mode\r\n");
-    spiflash_set_qspi_flag(SPIFLASH);
-    spiflash_set_quad_mode(SPIFLASH);
-
-    puts("Initialised!\r\n");
-
     puts("SoC type: ");
     puthex(SOC_ID->type);
     // This would make the golden reference output change every commit
@@ -62,27 +52,29 @@ void main() {
     // puthex(SOC_ID->version);
     puts("\r\n");
 
-    GPIO_1->oe = 0xF0;
-    GPIO_1->out = 0x50;
-    GPIO_1->out = 0xA0;
-    GPIO_1->oe = 0x00;
+    GPIO_1->mode = GPIO_PIN4_PUSH_PULL | GPIO_PIN5_PUSH_PULL \
+                 | GPIO_PIN6_PUSH_PULL | GPIO_PIN7_PUSH_PULL;
+    GPIO_1->output = 0x50;
+    GPIO_1->setclr = GPIO_PIN4_CLEAR | GPIO_PIN5_SET \
+                   | GPIO_PIN6_CLEAR | GPIO_PIN7_SET;
+    GPIO_1->mode = GPIO_PIN4_INPUT_ONLY | GPIO_PIN5_INPUT_ONLY \
+		 | GPIO_PIN6_INPUT_ONLY | GPIO_PIN7_INPUT_ONLY;
 
     uart_puts(UART_1, "ABCD");
-    
+
     MOTOR_PWM0->numr = 0x1F;
     MOTOR_PWM0->denom = 0xFF;
     MOTOR_PWM0->conf = 0x3;
 
-    
     MOTOR_PWM1->numr = 0x3F;
     MOTOR_PWM1->denom = 0xFF;
     MOTOR_PWM1->conf = 0x3;
 
-
     MOTOR_PWM9->numr = 0x7F;
     MOTOR_PWM9->denom = 0xFF;
     MOTOR_PWM9->conf = 0x3;
-	
+
+    /*
     PDM0->outval = 0xFF;
     PDM1->outval = 0x7F;
     PDM2->outval = 0x3F;
@@ -90,11 +82,12 @@ void main() {
     PDM0->conf = 0x1;
     PDM1->conf = 0x1;
     PDM2->conf = 0x0;
-	
+    */
+
     puts("GPIO: ");
-    puthex(GPIO_1->in);
+    puthex(GPIO_1->input);
     puts(" ");
-    puthex(GPIO_1->in);
+    puthex(GPIO_1->input);
     puts("\n");
 
     puts("UART1: ");
@@ -110,7 +103,7 @@ void main() {
     // test 8 bit transfer
     puthex(spi_xfer(USER_SPI_0, 0x5A, 8));
     puts(" ");
-    // test an odd 21 bit transfer 
+    // test an odd 21 bit transfer
     puthex(spi_xfer(USER_SPI_0, 0x123456, 21));
     puts("\n");
 
