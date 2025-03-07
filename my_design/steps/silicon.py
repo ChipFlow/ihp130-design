6aeeeb6409d5048f1b301630fd3197c71e101d4d
Rob Taylor <rob.taylor@chipflow.io>
2025-03-07 11:27:34 +0000

    Remove old silicon.py/silicon2.py

--- my_design/steps/silicon.py
+++
@@ -1,7 +0,0 @@
-from chipflow_lib.steps.silicon import SiliconStep
-from ._chipflow_top import ChipflowTop
-
-class MySiliconStep(SiliconStep):
-    def prepare(self):
-        return self.platform.build(ChipflowTop(), name="testchip_top")
-
